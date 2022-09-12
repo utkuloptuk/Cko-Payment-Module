e512c95424e1fc3e6b8a3500a93f4681cd065b70
UtkuLoptuk <utkuloptuk@gmail.com>
2022-09-12 14:02:52 +0300

    Added Cors settings in middleware and styplecop analyzer json file.

---
+++ Cko-Payment-Module/Extensions/ServiceExtensions.cs
@@ -0,0 +1,27 @@
+﻿// <copyright file="ServiceExtensions.cs" company="PlaceholderCompany">
+// Copyright (c) PlaceholderCompany. All rights reserved.
+// </copyright>
+
+namespace Cko_Payment_Module.Extensions
+{
+    /// <summary>
+    /// Service Extension class.
+    /// </summary>
+    public static class ServiceExtensions
+    {
+        /// <summary>
+        /// Cors configuration class.There we can change accessbility.
+        /// </summary>
+        /// <param name="services">Service.</param>
+        public static void ConfigureCors(this IServiceCollection services)
+        {
+            services.AddCors(options =>
+            {
+                options.AddPolicy("CorsPolicy", builder =>
+                builder.AllowAnyOrigin()
+                .AllowAnyMethod()
+                .AllowAnyHeader());
+            });
+        }
+    }
+}
